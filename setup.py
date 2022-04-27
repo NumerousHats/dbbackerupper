4840730aeee6baa203d09aefdde183cb4fdf9895
Michael Andrec <mandrecphd@gmail.com>
2022-04-27 16:51:38 -0400

    Turn off gtid

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/NumerousHats/dbbackerupper',
-    version='0.2.8',
+    version='0.2.9',
     zip_safe=False,
 )
